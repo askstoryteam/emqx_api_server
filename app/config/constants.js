b07412286972a3eb63c2b2c22ec12c171e6d022e
dhryoo <idfeelme@gmail.com>
2023-09-07 10:33:38 +0900

    before modify topic structure

--- app/config/constants.js
+++ app/config/constants.js
@@ -59,4 +59,5 @@ define('DB_CONNECTION_LIMIT',				1);
 define('DB_WAIT_FOR_CONNECTIONS',			true);
 define('DB_DEBUG',							false);
 define('API_SERVER_IP',					    '192.168.0.14');
+//define('API_SERVER_IP',					    '172.30.1.24');
 
